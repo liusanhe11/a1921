f397785beb325bebd28c0f230095775513e43846
root <root@LAPTOP-MFCQI2FT.localdomain>
2020-11-26 21:32:35 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+tpaixu:paixu.c testpaixu.c 
+	gcc paixu.c testpaixu.c -o tpaixu
+
+clean:
+	rm paixu
---
+++ paixu.h
@@ -0,0 +1,14 @@
+#ifndef paixu_h
+#define paixu_h
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void Bubble(int a[],int n);
+void Insert(int a[], int n);
+void Select(int a[], int n);
+void Quick(int a[], int n);                                                          
+
+#endif
+
+
